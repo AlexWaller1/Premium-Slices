3eac5471b3793cfa866b3c021afa849fe37c2308
Alex Waller <alex@artofalexwaller.com>
2021-01-18 21:12:28 -0500

    initial commit

--- app/controllers/art_controller.rb
+++ app/controllers/art_controller.rb
@@ -1,12 +1,12 @@
 class ArtController < ApplicationController
 
-    #get '/art/new' do
-    #if logged_in?
-        #erb :'/art/new'
-    #else
-        #redirect '/login'
-   #end
-#end
+    get '/art/new' do
+    if logged_in?
+        erb :'/art/new'
+    else
+        redirect '/login'
+   end
+end
 
 #post '/art' do
     #art = current_pizza.places.build(params)
@@ -22,7 +22,7 @@ class ArtController < ApplicationController
  get '/art' do
     if logged_in?
     
-        @places = Place.all.reverse
+        
         erb :'art/index'
     else
         redirect '/login'
