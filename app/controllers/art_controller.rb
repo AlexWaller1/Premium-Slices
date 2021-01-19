a7e418ef64cb128cc6dec21123a2ae65cfb5c762
Alex Waller <alex@artofalexwaller.com>
2021-01-19 00:11:49 -0500

    initial commit

--- app/controllers/art_controller.rb
+++ app/controllers/art_controller.rb
@@ -2,16 +2,7 @@ class ArtController < ApplicationController
 
    
 
-#post '/art' do
-    #art = current_pizza.places.build(params)
-     #if !place.title.empty? && !place.description.empty?
-         #place.save
-         #redirect '/art'
-     #else
-         #@error = "Hey! Give us more information!"
-         #erb :'art/new'
-     #end
- #end
+
 
  get '/art' do
     if logged_in?
@@ -23,13 +14,6 @@ class ArtController < ApplicationController
     end
 end
 
-#get '/places/:id' do
-    #if logged_in?
-        #@place = Place.find(params["id"])
-        #erb :'places/show'
-    #else
-        #redirect '/login'
-    #end
-#end
+
 
 end
