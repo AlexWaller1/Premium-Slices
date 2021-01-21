0c746cdb741302c698e2d9b21177073b5d1c3b97
Alex Waller <alex@artofalexwaller.com>
2021-01-21 13:25:23 -0500

    initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,16 +9,12 @@ class UsersController < ApplicationController
         # users should not be able to create an account with any blank credentials
         # users should not be able to create an account with a username that already exists
         user = User.new(params)
-        if user.username.empty? || user.password.empty?
-            @error = "Hey! Who Are You!?"
-            erb :'users/signup'
-        elsif User.find_by(username: user.username)
-            @error = "That's a Preexisting Account"
-            erb :'users/signup'
-        else
-            user.save
+        if user.save
             session[:user_id] = user.id
             redirect '/places'
+        else
+            @error = "Sorry, we need more information, or this may be a preexisting account."
+            erb :'/users/signup'
         end
     end
 
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,5 @@
 class Place < ActiveRecord::Base
     belongs_to :user
+    validates_presence_of :title, :method
+    validates_uniqueness_of :title
 end
